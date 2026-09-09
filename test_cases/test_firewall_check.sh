41d0765e84f5dc3a636666d16ee009409342d89c
sowmiya r <88027669+sowmiya34@users.noreply.github.com>
2026-09-09 13:45:25 +0530

    Create  test_firewall_check.sh

---
+++ test_cases/test_firewall_check.sh
@@ -0,0 +1,12 @@
+Test	Requirement	Marks
+TC01	Bash script exists and has shebang	1
+TC02	systemctl status firewalld used	2
+TC03	systemctl start firewalld used	2
+TC04	systemctl enable firewalld used	2
+TC05	firewall-cmd --state used	2
+TC06	firewall-cmd --get-default-zone used	2
+TC07	firewall-cmd --get-active-zones used	2
+TC08	Script executes successfully	2
+TC09	No destructive firewall commands	2
+TC10	Clear output/messages	1
+Total		18
